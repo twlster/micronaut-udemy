9f9af96cb3012aa97228903d9136a97bf9d0b462
twlster <twlster>
2023-11-27 15:26:13 +0000

    Initial commit

---
+++ src/main/java/edu/udemy/micronaut/Application.java
@@ -0,0 +1,18 @@
+package edu.udemy.micronaut;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "micronaut-udemy",
+            version = "0.0"
+    )
+)
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
